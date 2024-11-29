8808e7fcc516a2993b43b9172efccae842e1b882
mamamudang <humlake7.66@gmail.com>
2024-11-29 15:19:02 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main() {
+    int number;
+    int even_count = 0;
+    int odd_count = 0;
+
+    while (true) {
+        cout << "Enter an integer: ";
+        cin >> number;
+        
+        if (number == 0) {
+            break;
+        }
+
+  
+        if (number % 2 == 0) {
+            even_count++;
+        } else {
+            odd_count++;
+        }
+    }
+
+    cout << "#Even numbers = " << even_count << endl;
+    cout << "#Odd numbers = " << odd_count << endl;
+
     return 0;
 }
